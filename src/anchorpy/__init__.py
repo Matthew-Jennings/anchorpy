240b6925175f4d68228098201447cc4f33a91c77
Matthew Jennings <Centrus.007@gmail.com>
2021-07-26 17:44:01 +0930

    simplify top-level iomport

---
+++ src/anchorpy/__init__.py
@@ -0,0 +1,9 @@
+from .anchor import (
+    Anchor,
+    CONTRACT_ADDRESSES,
+    mnem_key_from_file,
+    pretty_format_coins,
+    PUBLIC_NODE_URLS,
+    uaust_to_uusd,
+    ubluna_to_uusd,
+)
--- tests/test_anchor.py
+++ tests/test_anchor.py
@@ -3,21 +3,21 @@ import pathlib
 from terra_sdk.client import lcd
 from terra_sdk.core import coin, coins, Dec
 
-from anchorman import anchor
+import anchorpy
 
 HERE = pathlib.Path(__file__).parent.resolve()
 ROOT = HERE.parent
 
 CHAIN_ID_TESTNET = "tequila-0004"
 LCD_TEST = lcd.LCDClient(
-    chain_id=CHAIN_ID_TESTNET, url=anchor.PUBLIC_NODE_URLS[CHAIN_ID_TESTNET]
+    chain_id=CHAIN_ID_TESTNET, url=anchorpy.PUBLIC_NODE_URLS[CHAIN_ID_TESTNET]
 )
 MNEM_PATH_TEST = ROOT / "mnemonic.txt"
-WALLET_TEST = LCD_TEST.wallet(anchor.mnem_key_from_file(MNEM_PATH_TEST))
+WALLET_TEST = LCD_TEST.wallet(anchorpy.mnem_key_from_file(MNEM_PATH_TEST))
 
 
 def test_anchor():
-    anchor_test = anchor.Anchor(LCD_TEST, WALLET_TEST.key.acc_address)
+    anchor_test = anchorpy.Anchor(LCD_TEST, WALLET_TEST.key.acc_address)
 
     BALANCE_EXPECTED = coins.Coins.from_str("3727582uluna,6489712uusd")
     assert anchor_test.balance == BALANCE_EXPECTED
@@ -29,13 +29,16 @@ def test_anchor():
     assert anchor_test.earn_balance_uusd > EARN_BALANCE_UUSD_EXPECTED
 
     BORROW_COLLATERAL_BALANCE_EXPECTED = coin.Coin("ubluna", Dec(358245650))
-    assert anchor_test.borrow_collateral_balance == BORROW_COLLATERAL_BALANCE_EXPECTED
+    assert (
+        anchor_test.borrow_collateral_balance_ubluna
+        == BORROW_COLLATERAL_BALANCE_EXPECTED
+    )
 
 
 def test_ubluna_to_uusd():
     ubluna_coin_to_exchange = coin.Coin("ubluna", int(1e6))
 
-    uusd_received = anchor.ubluna_to_uusd(LCD_TEST, ubluna_coin_to_exchange)
+    uusd_received = anchorpy.ubluna_to_uusd(LCD_TEST, ubluna_coin_to_exchange)
 
     print(uusd_received)
 
